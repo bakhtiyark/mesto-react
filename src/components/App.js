a1432f7b003395efe44d3ca52b1fc3c3fa3e8b20
Bakhtiyar K <bakhtiyark234@gmail.com>
2022-08-06 00:47:32 +0500

    PopupWithForm refactored per React

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,37 @@
 import Header from "./Header.js"
 import Main from "./Main.js"
 import Footer from "./Footer.js"
+import React from "react";
 
 
 
 
 function App() {
+  const [isAvatarPopupOpen, setAvatarPopupOpen] = React.useState(false)
+  const [isAddPlacePopupOpen, setAddPlacePopupOpen] = React.useState(false)
+  const [isProfilePopupOpen, setProfilePopupOpen] = React.useState(false)
+
+  function replaceAvatar() {
+    setAvatarPopupOpen(true)
+  }
+
+  function addPlace(){
+    setAddPlacePopupOpen(true)
+  }
+
+  function openProfilePopup(){
+    setProfilePopupOpen(true)
+  }
+
+  function closePopups(){
+    setAvatarPopupOpen(false)
+    setAddPlacePopupOpen(false)
+    setProfilePopupOpen(false)
+  }
+
+
+
+
   return (
     <div className="page">
       <Header />
--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -4,18 +4,14 @@ function PopupWithForm(props) {
     return (
         <div className={className}>
             <div className="popup__container">
+                <button type="button" onClick={props.Close} className="popup__close popup__close-icon" id="profile-close-button"></button>
                 <h2 className="popup__title">{props.title}</h2>
                 <form className="popup__form" name={props.name} noValidate>
-                    <input id="profile-name-input" className="popup__input popup__input_profile_name" type="text"
-                        name="profileFormName" minLength="2" maxLength="40" required />
-                    <span className="profile-name-input-error"></span>
-                    <input id="profile-secondary-input" className="popup__input popup__input_profile_secondary"
-                        type="text" name="profileFormSecondary" minLength="2" maxLength="200" required />
-                    <span className="profile-secondary-input-error"></span>
+                    {props.children}
                     <button type="submit" className="popup__save-button">{props.buttonText}</button>
                 </form>
-                <button type="button" onClick={props.Close} className="popup__close popup__close-icon" id="profile-close-button"></button>
             </div>
         </div>
     )
 }
+export default PopupWithForm;
